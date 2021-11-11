b2003b6610931c88aab3d9b2f56a8e0fe03a6781
Ramin Rakhshani <RaminRakhshani@outlook.com>
2021-11-11 03:37:59 +0330

    custome provider added

--- Index.js
+++ Index.js
@@ -1,6 +1,17 @@
 const Web3 = require('web3');
 
-const provider = new Web3.providers.HttpProvider('http://localhost:7545');
-const web3 = new Web3(provider);
+const customProvider = {
+    sendAsync: (payload, cb) => {
+        console.log('you called');
+        console.log(payload);
+        cb(undefined, 100);
+    }
+}
+
+// const provider = new Web3.providers.HttpProvider('http://localhost:7545');
+const web3 = new Web3(customProvider);
 // const web3 = new Web3('http://localhost:7545');
 //connected to the blockchain
+
+web3.eth.getBlockNumber()
+    .then(() => console.log('done'));
