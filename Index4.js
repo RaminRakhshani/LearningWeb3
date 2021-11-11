8a11f307c9319f84aebf5c28f9e3f96ab4f6f798
Ramin Rakhshani <RaminRakhshani@outlook.com>
2021-11-11 04:47:37 +0330

    ethereum send API implemented

---
+++ Index4.js
@@ -0,0 +1,31 @@
+const Web3 = require('web3');
+
+const MyContract = require('./MyContract.json');
+
+const init = async () => {
+    const web3 = new Web3('HTTP://127.0.0.1:7545');
+
+    const id = await web3.eth.net.getId();
+    const deployedNetwork = MyContract.networks[id];
+    const contract = new web3.eth.Contract(
+        MyContract.abi,
+        deployedNetwork.address
+    );
+
+    //get ganach addresses
+    const addresses = await web3.eth.getAccounts();
+
+    // console.log(addresses[0]);
+    //send transaction
+    const receipt = await contract.methods.setData(10).send({
+        from: addresses[0],
+        //gas: 100
+        //gasPrice: 100
+    });
+
+    const result = await contract.methods.getData().call();
+    console.log(receipt);
+    console.log(result);
+}
+
+init();
