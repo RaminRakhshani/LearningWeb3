6472fdd7712ab54f7a08ccc3cce5899098fe4bcb
Ramin Rakhshani <RaminRakhshani@outlook.com>
2021-11-11 04:50:30 +0330

    use then for promise method

--- Index4.js
+++ Index4.js
@@ -17,11 +17,17 @@ const init = async () => {
 
     // console.log(addresses[0]);
     //send transaction
-    const receipt = await contract.methods.setData(10).send({
+    contract.methods.setData(10).send({
         from: addresses[0],
         //gas: 100
         //gasPrice: 100
-    });
+    })
+        .then(receipt => {
+            //put your codes
+        })
+        .catch(error => {
+            //catch the error
+        });
 
     const result = await contract.methods.getData().call();
     console.log(receipt);
